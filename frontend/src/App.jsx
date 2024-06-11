46c1c0682878941852af407961594937a85cd3b7
Jorge Matos <jisapjisap@gmail.com>
2024-06-11 16:39:10 +0200

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Listing from './pages/Listing'
 import Favourites from './pages/Favourites'
 import Bookings from './pages/Bookings'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 
 export default function App() {
