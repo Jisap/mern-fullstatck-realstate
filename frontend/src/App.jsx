1745eb7591e9e6c92f23820061e0e4269169ce47
Jorge Matos <jisapjisap@gmail.com>
2024-06-11 16:22:04 +0200

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import AddProperty from './pages/AddProperty'
+import Listing from './pages/Listing'
+
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/listing" element={<Listing />} />
+        <Route path="/addproperty" element={<AddProperty />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
