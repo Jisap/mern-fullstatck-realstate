1d4dfd5df90e538257d7ab43d514ef1c7371c41a
Jorge Matos <jisapjisap@gmail.com>
2024-06-11 16:35:14 +0200

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import AddProperty from './pages/AddProperty'
 import Listing from './pages/Listing'
+import Favourites from './pages/Favourites'
 
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/listing" element={<Listing />} />
         <Route path="/addproperty" element={<AddProperty />} />
+        <Route path="/favourites" element={<Favourites />} />
       </Routes>
     </BrowserRouter>
   )
