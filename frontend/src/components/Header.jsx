838d402f49c5c0039e07586e0de3befc743f7fdb
Jorge Matos <jisapjisap@gmail.com>
2024-06-12 20:17:31 +0200

    header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+    <header>
+      {/* container */}
+      <div>
+        <div>
+          {/* logo */}
+          <Link to={'/'} className="flex items-center gap-x-2">
+            <span className='font-[900] text-[24px]'>Casa<span className='font-[600] medium-20'>Central</span></span>
+          </Link>
+        </div>
+      </div>
+    </header>
 }
 
 export default Header
