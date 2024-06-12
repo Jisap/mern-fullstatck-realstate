71d0496f043379bae2f64819a2edf414f6b27aa1
Jorge Matos <jisapjisap@gmail.com>
2024-06-12 20:59:43 +0200

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { MdHomeWork } from 'react-icons/md'
+import { MdHomeWork, MdPermContactCalendar, MdAddHome } from 'react-icons/md'
+import { RiCheckboxMultipleBlankFill } from "react-icons/ri";
 
-const Navbar = () => {
+const Navbar = ({containerStyles}) => {
   return (
     <nav>
       <NavLink
         to='/'
-        className={(isActive) => isActive ? 'active-link flexCenter gap-x-1 rounded-full' : 'flexCenter gap-x-1 rounded-full'}
+        className={({isActive}) => isActive ? 'active-link flexCenter gap-x-1 rounded-full px-2 py-1' : 'flexCenter gap-x-1 rounded-full px-2 py-1'}
       >
         <MdHomeWork />
         <div>Home</div>
+      </NavLink>
+      
+      <NavLink
+        to='/listing'
+        className={({isActive}) => isActive ? 'active-link flexCenter gap-x-1 rounded-full px-2 py-1' : 'flexCenter gap-x-1 rounded-full px-2 py-1'}
+      >
+        <RiCheckboxMultipleBlankFill />
+        <div>Listing</div>
+      </NavLink>
+      
+      <NavLink
+        to='/mailto:jisapjisap@gmail.com'
+        className={'flexCenter gap-x-1 rounded-full px-2 py-1 cursor-pointer'}
+      >
+        <MdPermContactCalendar />
+        <div>Contact</div>
+      </NavLink>
+
+      <NavLink
+        to='/'
+        className={ 'flexCenter gap-x-1 rounded-full px-2 py-1 cursor-pointer'}
+      >
+        <MdAddHome />
+        <div>Add property</div>
       </NavLink>
     </nav>
   )
