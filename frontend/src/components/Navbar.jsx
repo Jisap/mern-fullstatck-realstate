35564e27f11c42099e5a6aa07e50ddd24673c5e9
Jorge Matos <jisapjisap@gmail.com>
2024-06-12 20:41:07 +0200

    Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { MdHomeWork } from 'react-icons/md'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <NavLink
+        to='/'
+        className={(isActive) => isActive ? 'active-link flexCenter gap-x-1 rounded-full' : 'flexCenter gap-x-1 rounded-full'}
+      >
+        <MdHomeWork />
+        <div>Home</div>
+      </NavLink>
+    </nav>
   )
 }
 
