5758d89ae707c99f1d34c0f9f0a75ca0e63ee234
Jorge Matos <jisapjisap@gmail.com>
2024-06-12 20:29:57 +0200

    header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Navbar from './Navbar'
 
 const Header = () => {
   return (
-    <header>
+    <header className='max-padd-container fixed top-1 w-full left-0 right-0 z-50'>
       {/* container */}
-      <div>
-        <div>
+      <div className='max-padd-container bg-white transition-all duration-200 rounded-full px-5 ring-1 ring-slate-900/5'>
+        <div className='flexBetween py-3'>
           {/* logo */}
-          <Link to={'/'} className="flex items-center gap-x-2">
+          <Link to={'/'}>
             <span className='font-[900] text-[24px]'>Casa<span className='font-[600] medium-20'>Central</span></span>
           </Link>
+          {/* navbar */}
+          <div className=''>
+            {/* desktop */}
+            <Navbar />
+            {/* mobile */}
+            <Navbar />
+          </div>
+          {/* buttons */}
+          <div>
+            buttons & icons
+          </div>
         </div>
       </div>
     </header>
