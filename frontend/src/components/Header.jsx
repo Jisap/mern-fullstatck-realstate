8b41399f88a570d8994f0d109b6a279178665010
Jorge Matos <jisapjisap@gmail.com>
2024-06-13 18:48:36 +0200

    navbar mobile

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Navbar from './Navbar'
+import { MdMenu, MdClose } from 'react-icons/md'
 
 const Header = () => {
+
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpened(!menuOpened)
+
+  }
+
   return (
     <header className='max-padd-container fixed top-1 w-full left-0 right-0 z-50'>
       {/* container */}
@@ -15,13 +24,29 @@ const Header = () => {
           {/* navbar */}
           <div className='flexCenter gap-x-4'>
             {/* desktop */}
-            <Navbar containerStyles={'hidden xl:flex gap-x-5 xl:gap-x-10 capitalize medium-15'}/>
+            <Navbar containerStyles={'hidden xl:flex gap-x-5 xl:gap-x-10 capitalize medium-15 ring-1 ring-slate-900/10 rounded-full p-2 bg-primary'}/>
             {/* mobile */}
-            {/* <Navbar /> */}
+            <Navbar 
+              containerStyles={`${
+                menuOpened 
+                  ? "flex items-start flex-col gap-y-8 capitalize fixed top-20 right-8 p-12 bg-white rounded-3xl shadow-md w-64 medium-16 ring-1 ring-slate-900/5 transition-all duration-300 z-50" 
+                  : "flex items-start flex-col gap-y-8 capitalize fixed top-20 -right-[100%] p-12 bg-white rounded-3xl shadow-md w-64 medium-16 ring-1 ring-slate-900/5 transition-all duration-300"
+              }`}
+            />
           </div>
           {/* buttons */}
           <div>
-            buttons & icons
+            {!menuOpened ? (
+              <MdMenu 
+                className="xl:hidden cursor-pointer text-3xl hover:text-secondary"
+                onClick={toggleMenu}
+              />
+            ) : ( 
+              <MdClose 
+                className="xl:hidden cursor-pointer text-3xl hover:text-secondary"
+                onClick={toggleMenu}
+              />
+            )}
           </div>
         </div>
       </div>
