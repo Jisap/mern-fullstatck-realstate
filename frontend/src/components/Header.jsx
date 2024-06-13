3bf61bac703d179c88258520376c8eab44743c1f
Jorge Matos <jisapjisap@gmail.com>
2024-06-13 19:19:43 +0200

    header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Navbar from './Navbar'
 import { MdMenu, MdClose } from 'react-icons/md'
@@ -6,17 +6,34 @@ import userIcon from "../assets/user.svg"
 
 const Header = () => {
 
+  const [active, setActive] = useState(false);
+
   const [menuOpened, setMenuOpened] = useState(false);
 
   const toggleMenu = () => {
     setMenuOpened(!menuOpened)
-
   }
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY > 0) {  // Close menu if open when scrolling occurs
+        if(menuOpened) {
+          setMenuOpened(false)
+        }
+      }
+      setActive(window.scrollY > 40) // detect scroll
+    }
+    window.addEventListener("scroll", handleScroll)
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [menuOpened]);
 
   return (
     <header className='max-padd-container fixed top-1 w-full left-0 right-0 z-50'>
       {/* container */}
-      <div className='max-padd-container bg-white transition-all duration-200 rounded-full px-5 ring-1 ring-slate-900/5'>
+      <div className={`${active ? "py-0" : "py-1" } max-padd-container bg-white transition-all duration-200 rounded-full px-5 ring-1 ring-slate-900/5`}>
         <div className='flexBetween py-3'>
           {/* logo */}
           <Link to={'/'}>
