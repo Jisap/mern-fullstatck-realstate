74215c9b15bec17e72d8b4a0674f4c53dbabc6c6
Jorge Matos <jisapjisap@gmail.com>
2024-06-15 19:45:40 +0200

    banner

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero'
 import About from '../components/About'
 import Properties from '../components/Properties'
 import Blogs from '../components/Blogs'
+import bannerImg from '../assets/banner.png'
 
 const Home = () => {
   return (
@@ -11,6 +12,9 @@ const Home = () => {
       <About />
       <Properties />
       <Blogs />
+      <div className="max-padd-container py-16 overflow-x-hidden">
+        <img src={bannerImg} alt="" />
+      </div>
     </main>
   )
 }
