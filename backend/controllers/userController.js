061018cf84f3e3f1ede58f68d5d339be57f19315
Jorge Matos <jisapjisap@gmail.com>
2024-06-17 19:59:36 +0200

    userRouter

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,5 +5,19 @@ export const createUser = asyncHandler(async(req, res) => {
   console.log("creating user");
 
   let {email} = req.body;
-  console.log(email)
+  //console.log(email)
+
+  const userExists = await prisma.user.findUnique({where: {email: email}});
+
+  if(!userExists){
+    const user = await prisma.user.create({data: req.body});
+    res.send({
+      message: "user registered successfully",
+      user: user,
+    })
+  }else{
+    res.status(201).send({
+      message: "user already registered"
+    }) 
+  }
 })
