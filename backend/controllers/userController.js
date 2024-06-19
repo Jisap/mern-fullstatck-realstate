d4bd7ea19840815d5f1e6ea8057133c13dd42253
Jorge Matos <jisapjisap@gmail.com>
2024-06-19 19:56:20 +0200

    allFav

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -148,6 +148,24 @@ export const toFav = asyncHandler(async(req, res) => {
   } catch (err) {
     throw new Error(err.message)
   }
+});
+
+// To get all favourites list
+export const allFav = asyncHandler(async(req, res) => {
+  const { email } = req.body
+
+  try {
+    const favResd = await prisma.user.findUnique({
+      where: {email},
+      select: {favResidenciesID: true}
+    });
+
+    res.status(200).send(favResd)
+    
+  } catch (err) {
+    throw new Error(err.message)
+  }
+
 })
 
 
