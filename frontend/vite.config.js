d6545b67a05dcf0406f36d7568d362eb91154a07
Jorge Matos <jisapjisap@gmail.com>
2024-06-23 22:17:37 +0200

    openCage Geocoder & Vite config envs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,10 +8,10 @@ import react from '@vitejs/plugin-react'
 // })
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), ''); // Carga las variables de entorno según el modo (development, production)
   return {
     define: {
-      'process.env': env
+      'process.env': env                        // Establece `process.env` con las variables de entorno cargadas
     },
     plugins: [react()],
   }
