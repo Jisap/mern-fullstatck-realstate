f9b24d869c3e79f52fe94ed811293a76c191a144
Jorge Matos <jisapjisap@gmail.com>
2024-07-03 17:37:11 +0200

    uploadImage

--- frontend/src/components/UploadImage.jsx
+++ frontend/src/components/UploadImage.jsx
@@ -1,15 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { MdOutlineCloudUpload } from 'react-icons/md'
+import { Group, Button } from '@mantine/core'
 
 const UploadImage = ({ nextStep, prevStep, propertyDetails, setPropertyDetails }) => {
   
   const[imageURL, setImageURL] = useState(propertyDetails.image)
+  const cloudinaryRef = useRef()
+  const widgetRef = useRef()
+
+  useEffect(() => {
+    cloudinaryRef.current = window.cloudinary;
+    widgetRef.current = cloudinaryRef.current.createUploadWidget(
+      {
+        cloudName: "downe22q2",
+        uploadPreset: "hjr3nguo",
+        maxFile: 1
+      },
+      (err, result) => {
+        if(result.event === "success"){
+          setImageURL(result.info.secure_url)
+        }
+      }
+    )
+  })
 
   return (
-    <div>
+    <div className="mt-12 flexCenter flex-col">
       {
         !imageURL ? (
-          <div className="flexCenter flex-col w-3/4 h-[21rem] border-dashed border-2 cursor-pointer">
+          <div 
+            onClick={() => widgetRef.current?.open()}
+            className="flexCenter flex-col w-3/4 h-[21rem] border-dashed border-2 cursor-pointer"
+          >
             <MdOutlineCloudUpload 
               size={44}
               color="grey"
@@ -17,11 +39,19 @@ const UploadImage = ({ nextStep, prevStep, propertyDetails, setPropertyDetails }
             <span>Upload Image</span>
           </div>
         ) : (
-          <div>
-            
+          <div
+            onClick={() => widgetRef.current?.open()} 
+            className="w-3/4 h-[22rem] rounded-xl cursor-pointer overflow-hidden"
+          >
+              <img src={imageURL} alt="" className="h-full w-full object-cover"/>
           </div>
         )
       }
+
+      <Group justify="center" mt="xl">
+        <Button onClick={prevStep}>Go Back</Button>
+        <Button onClick={nextStep}>Next</Button>
+      </Group>
     </div>
   )
 }
