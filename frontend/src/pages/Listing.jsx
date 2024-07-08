56ba11860044bac7b7808de311bdd6292f4d57da
Jorge Matos <jisapjisap@gmail.com>
2024-07-08 20:25:12 +0200

    Search

--- frontend/src/pages/Listing.jsx
+++ frontend/src/pages/Listing.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Searchbar from '../components/Searchbar'
 //import { PROPERTIES } from '../constants/data'
 import Item from "../components/Item";
@@ -8,6 +8,7 @@ import { PuffLoader } from 'react-spinners'
 const Listing = () => {
 
   const {data, isError, isLoading} = useProperties();
+  const [filter, setFilter] = useState("");
 
   if(isError){
     return(
@@ -35,12 +36,24 @@ const Listing = () => {
     <main className="max-padd-container my-[99px]">
       <div className="max-padd-container py-10 xl:py-22 bg-primary rounded-3xl">
         <div>
-          <Searchbar />
+          <Searchbar
+            filter={filter}
+            setFilter={setFilter}
+          />
           {/* container */}
           <div className="grid gap-6 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mt-10">
-            {data.map((property) => (
+            {
+              data.filter((property) => (
+                property.title.toLowerCase().includes(filter.toLowerCase()) ||
+                property.city.toLowerCase().includes(filter.toLowerCase()) ||
+                property.country.toLowerCase().includes(filter.toLowerCase())
+              )).map((property) => (
                 <Item key={property.title} property={property} />   
-            ))}
+              ))
+            }
+                
+              
+              
           </div>
         </div>
       </div>
